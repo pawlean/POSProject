20640c9b6bf9576e68c9e825c7265c6cec147347
Sergio José Vázquez Aguayo <sergiovazag@gmail.com>
2021-11-16 23:06:23 +0000

    Added Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
 	return (
-		<div>
-			<h1>Hola mundo</h1>
-		</div>
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Hola mundo</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
 	);
 };
 
